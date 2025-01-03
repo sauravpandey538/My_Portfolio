f195c69c7374002e07711bcf56c947589d0627c7
sauravpandey538 <sauravpandey0325@icloud.com>
2025-01-03 20:37:16 +0545

    flirting

--- src/App.jsx
+++ src/App.jsx
@@ -16,37 +16,55 @@ function App() {
   const { theme } = useTheme();
 
   return (
-    <Box>
-      <Box
-        h={"max-content"}
-        boxShadow={
-          theme.bg_color === "white"
-            ? "1px 1px 20px 1px lightgray"
-            : "1px 1px 20px 1px #2E2E2E"
-        }
-        position={"sticky"}
-        top={0}
-        right={0}
-        zIndex={999}
-      >
-        <Navigation />
-      </Box>
+    // <Box>
+    //   <Box
+    //     h={"max-content"}
+    //     boxShadow={
+    //       theme.bg_color === "white"
+    //         ? "1px 1px 20px 1px lightgray"
+    //         : "1px 1px 20px 1px #2E2E2E"
+    //     }
+    //     position={"sticky"}
+    //     top={0}
+    //     right={0}
+    //     zIndex={999}
+    //   >
+    //     <Navigation />
+    //   </Box>
 
-      <ShadowBox children={<MyDetail profile={profile} />} />
-      <ShadowBox children={<AboutMe />} />
-      <ShadowBox children={<ProjectMainPage profile={profile} />} />
-      <ShadowBox
-        children={
-          <>
-            {" "}
-            <ContactMe />
-            <Footer />{" "}
-          </>
-        }
-      />
-      <TranslateBtn />
-      <ThemeBtn />
-    </Box>
+    //   <ShadowBox children={<MyDetail profile={profile} />} />
+    //   <ShadowBox children={<AboutMe />} />
+    //   <ShadowBox children={<ProjectMainPage profile={profile} />} />
+    //   <ShadowBox
+    //     children={
+    //       <>
+    //         {" "}
+    //         <ContactMe />
+    //         <Footer />{" "}
+    //       </>
+    //     }
+    //   />
+    //   <TranslateBtn />
+    //   <ThemeBtn />
+    // </Box>
+    <>
+      <div
+        style={{
+          height: "100vh",
+          width: "100vw",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXtBCAu1d6_y40S5jdCe5UCsmgJ2rLGwXAeQ&s"
+          alt="Centered"
+        />
+        <h1 style={{ color: "black" }}>Sandhya ji 🥰</h1>
+      </div>
+    </>
   );
 }
 
